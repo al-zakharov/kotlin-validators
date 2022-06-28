f817461dd3cc109f2129d9ef35476fe6c519191a
Aleksandr Zakharov <github@alzakharov.com>
2022-06-28 19:28:52 +0300

    Added date validation

--- src/main/kotlin/com/alzakharov/ValidationFilters.kt
+++ src/main/kotlin/com/alzakharov/ValidationFilters.kt
@@ -1,7 +1,18 @@
 package com.alzakharov
 
+import com.alzakharov.filters.FilterDate
 import com.alzakharov.filters.FilterEmail
 
 object ValidationFilters {
+    /**
+     * Email validator
+     */
     fun email(email: String) = FilterEmail.invoke(email)
+
+    /**
+     * Date validator
+     *
+     * @return list of formats for validation
+     */
+    fun date() = FilterDate
 }
---
+++ src/main/kotlin/com/alzakharov/filters/FilterDate.kt
@@ -0,0 +1,261 @@
+package com.alzakharov.filters
+
+import com.alzakharov.filters.date.*
+
+object FilterDate : Filter {
+    /**
+     * Format validation: m/d/yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatUs(date: String) = Us.invoke(date)
+
+    /**
+     * Format validation: dd/mm/yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatUk(date: String) = Uk.invoke(date)
+
+    /**
+     * Format validation: dd/mm/yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatVn(date: String) = Uk.invoke(date)
+
+    /**
+     * Format validation: dd/mm/yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatId(date: String) = Uk.invoke(date)
+
+    /**
+     * Format validation: dd/mm/yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatEs(date: String) = Uk.invoke(date)
+
+    /**
+     * Format validation: dd/mm/yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatIt(date: String) = Uk.invoke(date)
+
+    /**
+     * Format validation: dd/mm/yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatFr(date: String) = Uk.invoke(date)
+
+    /**
+     * Format validation: d/m/yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatBr(date: String) = Uk.invoke(date)
+
+    /**
+     * Format validation: d/m/yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatGr(date: String) = Uk.invoke(date)
+
+    /**
+     * Format validation: d/m/yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatTh(date: String) = Uk.invoke(date)
+
+    /**
+     * Format validation: dd.mm.yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatDe(date: String) = De.invoke(date)
+
+    /**
+     * Format validation: dd.mm.yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatBg(date: String) = De.invoke(date)
+
+    /**
+     * Format validation: dd.mm.yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatNo(date: String) = De.invoke(date)
+
+    /**
+     * Format validation: dd.mm.yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatRo(date: String) = De.invoke(date)
+
+    /**
+     * Format validation: dd.mm.yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatRu(date: String) = De.invoke(date)
+
+    /**
+     * Format validation: dd.mm.yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatSi(date: String) = De.invoke(date)
+
+    /**
+     * Format validation: dd.mm.yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatTr(date: String) = De.invoke(date)
+
+    /**
+     * Format validation: dd.mm.yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatUa(date: String) = De.invoke(date)
+
+    /**
+     * Format validation: dd.mm.yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatLv(date: String) = De.invoke(date)
+
+    /**
+     * Format validation: dd-mm-yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatDk(date: String) = Dk.invoke(date)
+
+    /**
+     * Format validation: dd-mm-yyyy
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatPt(date: String) = Dk.invoke(date)
+
+    /**
+     * Format validation: yyyy/m/d
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatHk(date: String) = Hk.invoke(date)
+
+    /**
+     * Format validation: yyyy/m/d
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatTw(date: String) = Hk.invoke(date)
+
+    /**
+     * Format validation: yyyy-m-d
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatCn(date: String) = Hk.invoke(date)
+
+    /**
+     * Format validation: yyyy/mm/dd
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatIr(date: String) = Hk.invoke(date)
+
+    /**
+     * Format validation: yyyy/mm/dd
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatJp(date: String) = Hk.invoke(date)
+
+    /**
+     * Format validation: yyyy-mm-dd
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatCa(date: String) = Ca.invoke(date)
+
+    /**
+     * Format validation: yyyy-mm-dd
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatPl(date: String) = Ca.invoke(date)
+
+    /**
+     * Format validation: yyyy-mm-dd
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatSe(date: String) = Ca.invoke(date)
+
+    /**
+     * Format validation: yyyy-mm-dd
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatLt(date: String) = Ca.invoke(date)
+
+    /**
+     * Format validation: yyyy.mm.dd
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatHu(date: String) = Ca.invoke(date)
+
+    /**
+     * Format validation: ISO-8061
+     *
+     * @param date string to validate
+     * @return boolean match
+     */
+    fun formatIso8601(date: String) = Iso8601.invoke(date)
+}
